0d7f6b9ce84a246afa0bc32f81795d64c41e6180
surcouf <surcouf@git.com>
2025-09-14 21:01:35 +0200

    add: need to find out how to read raw packets

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <strings.h>
+#include <string.h>
 #include <signal.h>
 #include <arpa/inet.h>
 #include <netpacket/packet.h>
@@ -20,23 +21,27 @@ int main() {
 
 	sigaction(SIGINT, &action, NULL);
 
-	int protocol = htons(ETHERTYPE_IP);
-	int sock = socket(PF_PACKET, SOCK_RAW, protocol);
+	uint16_t protocol = htons(ETH_P_ALL);
+	printf("Prototcol: %x(%x)\n", protocol, ntohs(ETH_P_ALL));
+	//int sock = socket(PF_PACKET, SOCK_RAW, protocol);
+	int sock = socket(PF_INET, SOCK_RAW, IPPROTO_UDP);
 	if (sock == -1) {
 		perror("socket()");
 		return (EXIT_FAILURE);
 	}
 	printf("Socket:[%d]\n", sock);
 
-	
 	char *buffer = calloc(1001, 1);
 	int n = 0;
 	while ((n = recvfrom(sock, buffer, 1000, 0, NULL, NULL)) > 0) {
 		buffer[n] = 0;
-		printf("Raw packet:%s\n", buffer);
-		for (int i = 0; buffer[i]; i++)
-			buffer[i] = 0;
+		struct ethhdr *eth = (struct ethhdr *)(buffer);
+		printf("\t|Source adress: %.2X-%.2X-%.2X-%.2X-%.2X-%.2X\n", eth->h_source[0], eth->h_source[1], eth->h_source[2], eth->h_source[3], eth->h_source[4], eth->h_source[5]);
+		printf("Raw packet (size:%d):%s\n", n, buffer);
+		memset(buffer, 0, 1001);
+		//break ;
 	}
 	printf("Process exited with: %d bytes reads\n", n);
+	free(buffer);
 	return (0);
 }
