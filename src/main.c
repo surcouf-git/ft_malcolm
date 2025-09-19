c54c1b0d570d1100ea1922a015cd478fb09330f1
surcouf <surcouf@git.com>
2025-09-19 13:42:48 +0200

    add: ethernet frame struct

--- src/main.c
+++ src/main.c
@@ -14,8 +14,23 @@ void sig_handler(int) {
 	exit(0);
 }
 
+typedef struct arp_s {
+	uint16_t hardware_type;
+	uint16_t protocol_type;
+	unsigned char vide[38];
+} arp_t;
+
+typedef struct frame_s {
+	unsigned char destination[6];
+	unsigned char source[6];
+	uint16_t length_type;
+	arp_t arp;
+	int checksum;
+} frame_t;
+
 int main() {
 
+	printf("%ld\n", sizeof(arp_t));
 	struct sigaction action;
 	action.sa_handler = &sig_handler;
 
@@ -23,23 +38,28 @@ int main() {
 
 	uint16_t protocol = htons(ETH_P_ALL);
 	printf("Prototcol: %x(%x)\n", protocol, ntohs(ETH_P_ALL));
-	//int sock = socket(PF_PACKET, SOCK_RAW, protocol);
-	int sock = socket(PF_INET, SOCK_RAW, IPPROTO_UDP);
+	int sock = socket(AF_PACKET, SOCK_RAW, protocol);
 	if (sock == -1) {
 		perror("socket()");
 		return (EXIT_FAILURE);
 	}
 	printf("Socket:[%d]\n", sock);
 
-	char *buffer = calloc(1001, 1);
+	char *buffer = calloc(10000, 1);
 	int n = 0;
-	while ((n = recvfrom(sock, buffer, 1000, 0, NULL, NULL)) > 0) {
+	while ((n = recvfrom(sock, buffer, 10000, 0, NULL, NULL)) > 0) {
 		buffer[n] = 0;
-		struct ethhdr *eth = (struct ethhdr *)(buffer);
-		printf("\t|Source adress: %.2X-%.2X-%.2X-%.2X-%.2X-%.2X\n", eth->h_source[0], eth->h_source[1], eth->h_source[2], eth->h_source[3], eth->h_source[4], eth->h_source[5]);
-		printf("Raw packet (size:%d):%s\n", n, buffer);
-		memset(buffer, 0, 1001);
-		//break ;
+		frame_t *f = (frame_t *)buffer;
+		for (int i = 0; i < 6; i++) {
+			printf("%X ", f->destination[i]);
+		}
+		f->length_type = ntohs(f->length_type);
+		if (f->length_type == ETH_P_ARP) {
+			arp_t *arp = &f->arp;
+			printf("HW Type: %d | %d", arp->hardware_type, arp->protocol_type);
+		}
+		printf("\n");
+		memset(buffer, 0, 10000);
 	}
 	printf("Process exited with: %d bytes reads\n", n);
 	free(buffer);
